0578b180ffaf337b4f0ef5041be76e8a8d0e0a16
Ana <anaclimgo@gmail.com>
2026-04-02 15:49:06 -0300

    feat: added email to devise permit in signup

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::API
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email])
     devise_parameter_sanitizer.permit(:sign_in, keys: [:login])
   end
 end
